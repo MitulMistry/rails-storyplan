8ef0d1dffc59d516831bc9e754cc67a1967433e0
Mitul Mistry <mitul.mistry1@gmail.com>
2016-03-21 21:01:18 +0000

    fixed character creation in chapter form

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -3,7 +3,7 @@ class Chapter < ActiveRecord::Base
   has_many :character_chapters
   has_many :characters, through: :character_chapters
 
-  accepts_nested_attributes_for :characters, reject_if: :all_blank #allows creating characters in the chapter creation form
+  #accepts_nested_attributes_for :characters #, reject_if: :all_blank #allows creating characters in the chapter creation form
 
   validates :name, presence: true
   validates :story_id, presence: true
@@ -19,12 +19,12 @@ class Chapter < ActiveRecord::Base
   end
 
   #custom writer/setter for use during mass assignment from form
-  def character_attributes=(character_attributes)
-    if !character_attributes.empty?
-      character_attributes.values.each do |character_attribute|
-        character = Character.all.find_or_create_by(character_attribute)
-        post_tag = self.character_chapters.build(character: character) #using build on self, it already sets the chapter_id to this post for the character_chapter it's building
-        post_tag.save
+  def characters_attributes=(characters_attributes)
+    if !characters_attributes.nil?
+      characters_attributes.values.each do |character_attribute|
+        character = self.user.characters.find_or_create_by(character_attribute) #prevent duplicate characters for user #Character.find_or_create_by(character_attribute)
+        character_chapter = self.character_chapters.build(character: character) #using build on self, it already sets the chapter_id to this chapter for the character_chapter it's building
+        character_chapter.save
       end
     end
   end
