7e7355170a099e5791bd28c99b768ae8ff515abb
MitulMistry <MitulMistryDev@gmail.com>
2016-04-20 18:59:38 -0500

    adjusting collection grid

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,15 +19,30 @@ module ApplicationHelper
   def collection_grid_render_partial(item)
     item_class = item.class
     if item_class == Audience
-      render partial: 'audiences/audience', locals: { audience: item }
+      render partial: 'audiences/audience', locals: { item: item }
     elsif item_class == Character
-      render partial: 'characters/character', locals: { character: item }
+      render partial: 'characters/character', locals: { item: item }
     elsif item_class == Genre
-      render partial: 'genres/genre', locals: { genre: item }
+      render partial: 'genres/genre', locals: { item: item }
     elsif item_class == Story
-      render partial: 'stories/story', locals: { story: item }
+      render partial: 'stories/story', locals: { item: item }
     elsif item_class == Writer
-      render partial: 'writers/writer', locals: { writer: item }
+      render partial: 'writers/writer', locals: { item: item }
+    end
+  end
+
+  def collection_grid_partial_to_render(collection)
+    item_class = collection.first.class
+    if item_class == Audience
+      'audiences/audience'
+    elsif item_class == Character
+      'characters/character'
+    elsif item_class == Genre
+      'genres/genre'
+    elsif item_class == Story
+      'stories/story'
+    elsif item_class == Writer
+      'writers/writer'
     end
   end
 
