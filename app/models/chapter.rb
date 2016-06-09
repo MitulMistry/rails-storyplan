8e586cfde4df1698a8e94f1aee23efa1bdb8e6e2
Mitul Mistry <mitulmistrydev@gmail.com>
2016-06-09 16:12:27 +0000

    chapter model accepts_nested_attributes experiment

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -5,7 +5,13 @@ class Chapter < ActiveRecord::Base
 
   delegate :user, to: :story #exposes story's user method to be used by chapter (get story's user) - equivalent to: def user; self.story.user; end
 
-  #accepts_nested_attributes_for :characters #, reject_if: :all_blank #allows creating characters in the chapter creation form
+  #accepts_nested_attributes_for :characters, reject_if: all_blank
+
+  #self doesn't work: because macro goes into Chapter Class, not instance
+  # accepts_nested_attributes_for(:characters, reject_if: proc do  |attributes|
+  #   binding.pry
+  #    attributes['name'].blank? || self.user.characters.find_by(character_attribute)
+  #  end ) #allows creating characters in the chapter creation form
 
   validates :name, presence: true
   validates :story_id, presence: true
@@ -24,7 +30,7 @@ class Chapter < ActiveRecord::Base
       characters_attributes.values.each do |character_attribute|
         character = self.user.characters.find_or_create_by(character_attribute) #prevent duplicate characters for user #Character.find_or_create_by(character_attribute)
         character_chapter = self.character_chapters.build(character: character) #using build on self, it already sets the chapter_id to this chapter for the character_chapter it's building
-        character_chapter.save
+        character_chapter.save #using build instead of create because parent (chapter) isn't saved yet
       end
     end
   end
