cf75d5a74c9aac5fe275f5e7d2081629d50ab4af
MitulMistry <MitulMistryDev@gmail.com>
2017-05-02 17:53:07 -0500

    Implement search feature in controller (incomplete - failing tests)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,10 @@
 class SearchesController < ApplicationController
   def search
+    search_term = params[:search]
+    users = User.where("username ILIKE ? OR full_name ILIKE ?", "%#{search_term}", "%#{search_term}")
+    stories = Story.where("name ILIKE ?", "%#{search_term}%")
+    characters = Character.where("name ILIKE ?", "%#{search_term}")
+
+    @items = users + stories + characters #.order("created_at DESC")
   end
 end
