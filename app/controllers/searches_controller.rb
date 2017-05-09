6571d0f4f577f2b8f36fd96c68a39e1e15b548a6
MitulMistry <MitulMistryDev@gmail.com>
2017-05-08 19:48:48 -0500

    Add Kaminari pagination for search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def search #returns PgSearch::Document instances - user item.searchable to get the referring object
-    @items = PgSearch.multisearch(params[:search]).reorder("created_at DESC")
+    @items = PgSearch.multisearch(params[:search]).reorder("created_at DESC").page(params[:page]) #kaminari
   end
 end
