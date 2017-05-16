9da8ed20b295d42c617702ac919d6c36316f23dc
MitulMistry <MitulMistryDev@gmail.com>
2017-05-16 12:56:43 -0500

    Switch from Bootstrap card columns to Masonry grids

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   shared_examples_for "public access to pages" do
     describe "GET #index" do
-      it "populates an array of 3 stories" do
-        4.times { create(:story) }
+      it "populates an array of 4 stories" do
+        5.times { create(:story) }
         get :index
         expect(assigns(:stories).first).to be_a(Story)
-        expect(assigns(:stories).length).to eq 3
+        expect(assigns(:stories).length).to eq 4
       end
 
-      it "populates an array of 3 writers" do
-        4.times { create(:user) }
+      it "populates an array of 4 writers" do
+        5.times { create(:user) }
         get :index
         expect(assigns(:writers).first).to be_a(User)
-        expect(assigns(:writers).length).to eq 3
+        expect(assigns(:writers).length).to eq 4
       end
 
-      it "populates an array of 3 characters" do
-        4.times { create(:character) }
+      it "populates an array of 4 characters" do
+        5.times { create(:character) }
         get :index
         expect(assigns(:characters).first).to be_a(Character)
-        expect(assigns(:characters).length).to eq 3
+        expect(assigns(:characters).length).to eq 4
       end
 
       it "renders the :index template" do
