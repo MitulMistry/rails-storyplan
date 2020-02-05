bc693cc2edc61ebb702bd910be1f9eee3c21c602
Mitul Mistry <MitulMistryDev@gmail.com>
2020-02-05 15:52:05 -0600

    Update Ruby version, Bundler, and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,124 +10,124 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502230144) do
+ActiveRecord::Schema.define(version: 2017_05_02_230144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "audiences", force: :cascade do |t|
-    t.string   "name"
+  create_table "audiences", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "chapters", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "ch_number"
-    t.string   "objective"
-    t.integer  "target_word_count"
-    t.text     "overview"
-    t.boolean  "currently_writing", default: false
-    t.integer  "story_id"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+  create_table "chapters", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "ch_number"
+    t.string "objective"
+    t.integer "target_word_count"
+    t.text "overview"
+    t.boolean "currently_writing", default: false
+    t.integer "story_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "character_chapters", force: :cascade do |t|
-    t.integer  "character_id"
-    t.integer  "chapter_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "character_chapters", id: :serial, force: :cascade do |t|
+    t.integer "character_id"
+    t.integer "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "characters", force: :cascade do |t|
-    t.string   "name"
-    t.text     "bio"
-    t.string   "traits"
-    t.integer  "user_id"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.string   "portrait_file_name"
-    t.string   "portrait_content_type"
-    t.integer  "portrait_file_size"
+  create_table "characters", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "traits"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "portrait_file_name"
+    t.string "portrait_content_type"
+    t.bigint "portrait_file_size"
     t.datetime "portrait_updated_at"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "story_id"
-    t.text     "content"
+  create_table "comments", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "story_id"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "genres", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "genres", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text     "content"
-    t.string   "searchable_type"
-    t.integer  "searchable_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
+  create_table "pg_search_documents", id: :serial, force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.integer "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
-  create_table "stories", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "target_word_count"
-    t.text     "overview"
-    t.integer  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "cover_file_name"
-    t.string   "cover_content_type"
-    t.integer  "cover_file_size"
+  create_table "stories", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "target_word_count"
+    t.text "overview"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "cover_file_name"
+    t.string "cover_content_type"
+    t.bigint "cover_file_size"
     t.datetime "cover_updated_at"
   end
 
-  create_table "story_audiences", force: :cascade do |t|
-    t.integer  "story_id"
-    t.integer  "audience_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "story_genres", force: :cascade do |t|
-    t.integer  "story_id"
-    t.integer  "genre_id"
+  create_table "story_audiences", id: :serial, force: :cascade do |t|
+    t.integer "story_id"
+    t.integer "audience_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "story_genres", id: :serial, force: :cascade do |t|
+    t.integer "story_id"
+    t.integer "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "username"
-    t.text     "bio"
-    t.string   "full_name"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.text "bio"
+    t.string "full_name"
+    t.string "provider"
+    t.string "uid"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
